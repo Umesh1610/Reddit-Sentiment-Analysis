c604cac4979e124a681e632fbb568890cd084b7f
Umesh1610 <163689172+Umesh1610@users.noreply.github.com>
2025-04-01 13:18:14 -0500

    Update stream_reddit.py

--- stream_reddit.py
+++ stream_reddit.py
@@ -4,9 +4,9 @@ import json
 import time
 
 reddit = praw.Reddit(
-    client_id="aBcDeFgHiJkLmN",  # Replace with your Client ID
-    client_secret="xYz12345_aBcDeFgHiJkLmN",  # Replace with your Client Secret
-    user_agent="aws:RedditSentimentAnalysis:v1.0 by /u/yourusername"  # Replace with your User Agent
+    client_id="",  # Replace with your Client ID
+    client_secret="",  # Replace with your Client Secret
+    user_agent=""  # Replace with your User Agent
 )
 firehose = boto3.client('firehose')
 
